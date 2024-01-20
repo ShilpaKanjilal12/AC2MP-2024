a517538fdc75f506b245d806359c41f21b3f451d
Shilpa Kanjilal <skcodes29@gmail.com>
2024-01-20 15:16:40 +0530

    Updated about section

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 data = `
 <div class="footer text-center" style="background-color:rgb(238, 238, 238);
 padding: 5%;">
-&copy; <br/>Department of Physics, <br/> Indian Institute of Technology Patna
+&copy; AC2MP 2024 <br/>Department of Physics, <br/> Indian Institute of Technology Patna
 </div>`
 document.getElementById("footerContainer").innerHTML = data;
